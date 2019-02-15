7ed028d6b9da35e64ba59a2594264ee2e0cb9c34
Orlando Rodriguez <creativerodriguez@gmail.com>
2019-02-15 18:39:28 -0500

    home comp html scss ts updated
    home comp html scss ts updated
    look out for [] on input attributes for material examples

--- AngularPwaPostOne/src/app/home/home.component.ts
+++ AngularPwaPostOne/src/app/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+export interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,6 +12,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  
+  autoControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
 
   constructor() { }
 
